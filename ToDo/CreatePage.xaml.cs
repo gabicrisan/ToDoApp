64e03f23cb9f239f87ec9b5d360612ffdb82a4aa
Gabriel Crisan <gabriel.crisan@outlook.com>
2021-01-03 20:28:53 +0200

    first commit

---
+++ ToDo/CreatePage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ToDo
+{
+    public partial class CreatePage : ContentPage
+    {
+        public List<ToDoItem> toDoItems;
+
+        public CreatePage()
+        {
+            toDoItems = new List<ToDoItem>();
+            InitializeComponent();
+        }
+        public void OnSave(object o, EventArgs e)
+        {
+            toDoItems.Add(
+                new ToDoItem(
+                    ToDoEntry.Text,
+                    Priority.Text,
+                    SetDueDate(
+                        Date.Date,
+                        Time.Time.Hours,
+                        Time.Time.Minutes,
+                        Time.Time.Seconds),
+                    false
+                    )
+                );
+            Clear();
+        }
+
+        private DateTime SetDueDate(DateTime date, int hour, int minute, int second)
+        {
+            DateTime retVal = new DateTime(
+                date.Year,
+                date.Month,
+                date.Day,
+                hour,
+                minute,
+                second
+                );
+            return retVal;
+        }
+
+        private void Clear()
+        {
+            ToDoEntry.Text = Priority.Text = String.Empty;
+            Date.Date = DateTime.Now;
+            Time.Time = new TimeSpan(
+                DateTime.Now.Hour,
+                DateTime.Now.Minute,
+                DateTime.Now.Second
+                );
+        }
+
+        public void OnCancel(object o, EventArgs e)
+        {
+
+        }
+        public void OnReview(object o, EventArgs e)
+        {
+            var tempList = toDoItems;
+            Clear();
+        }
+    }
+}
